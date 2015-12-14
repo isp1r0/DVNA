b62d342571ca8ac24dc0bdc6649c31052d58300c
claudio <noreply@github.com>
2015-12-13 23:39:08 -0800

    Update main configuration

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function(req, res) {
-  var resp=eval("("+req.query.name+")");
-  res.send('Respones<?br>'+resp);
+  var resp=eval("("+req.query.target+")");
+  res.send('Parameter Value:</br>'+resp);
   });
 app.listen(9000);
